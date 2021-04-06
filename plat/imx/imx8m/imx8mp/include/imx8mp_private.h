c14ffcffebcf106be30ecea5b7e05b53bc0249c9
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2021-04-07 06:01:36 +0800

    plat: imx8mp: Add imx8mp_private.h to the build
    Allows for exporting of FIP related methods cleanly in a private header.
    
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>

---
+++ plat/imx/imx8m/imx8mp/include/imx8mp_private.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef IMX8MP_PRIVATE_H
+#define IMX8MP_PRIVATE_H
+
+/*******************************************************************************
+ * Function and variable prototypes
+ ******************************************************************************/
+void plat_imx8mp_io_setup(void);
+
+#endif /* IMX8MP_PRIVATE_H */
